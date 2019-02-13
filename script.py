36ff1cd3ccc12cc3fb748bbdcf602e30e9e9c7a2
Singhbalbir <balbir4email@gmail.com>
2019-02-14 04:16:25 +0530

    section (3) solution added

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ today = []
 yesterday = []
 urlh_today = []
 urlh_yesterday = []
+categories_today = []
+categories_yesterday = []
 
 def getJSON(file, day):
     with open(file, 'r') as fp:
@@ -16,15 +18,19 @@ list_yesterday = getJSON('./yesterday.json', today)
 
 for item in list_today:
     urlh_today.append(item['urlh'])
+    categories_today.append(item['category'])
 
 for item in list_yesterday:
     urlh_yesterday.append(item['urlh'])
+    categories_yesterday.append(item['category'])
 
 # print(len(urlh_today))
 # print(len(urlh_yesterday))
 
 overlapped_urlh = list(set(urlh_today).intersection(urlh_yesterday))
-print(len(overlapped_urlh))
+print("1. No. of overlapped urlh: ", len(overlapped_urlh))
 
+# print(len(set(categories_today)))
+# print(len(set(categories_yesterday)))
 
-    
+print("3. No. of unique categories in both files: ", len(set(categories_today).intersection(categories_yesterday)))
